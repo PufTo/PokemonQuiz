5348f2bae9636d62abbcb86b0379c9787ba68448
gstanciu <giorgiana.stanciu@gxo.com>
2022-03-30 22:47:16 +0300

    add start quiz button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import StartApp from "./Components/StartApp/StartApp";
 import Quiz from "./Components/Quiz/Quiz";
 
 function App() {
-  console.log("hello world");
+  const [currentPage, setCurrentPage] = useState('startApp');
+  const handleClickStart = () => {
+    setCurrentPage('quiz');
+  }
+  const renderPage = (page) => {
+    switch (page) {
+      case 'startApp': {
+        return <StartApp onClick={handleClickStart} />
+      }
+
+      case 'quiz': {
+        return <Quiz />
+      }
+    }
+  }
   return (
     <div className="App">
-      <Quiz />
+      {renderPage(currentPage)}
     </div>
   );
 }
