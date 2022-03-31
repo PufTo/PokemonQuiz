739c0f1d476706b863e42128924ceeb21d4a4530
Nicodin <paurnic@yahoo.com>
2022-04-01 02:04:47 +0300

    quality of life updates for pokemon cards

--- src/Components/Quiz/EndQuiz.js
+++ src/Components/Quiz/EndQuiz.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Container } from "@mui/material";
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from "@mui/icons-material/Send";
 import styles from "./EndQuiz.module.css";
 
 export default function EndQuiz(props) {
@@ -12,8 +12,19 @@ export default function EndQuiz(props) {
 
   return (
     <Container className={styles.fullHeight}>
-      <p className={styles.scoreEndScreen} >{`Score:${finalScore}`}</p>
-      <Button className={styles.buttonEndScreen} variant="contained" size="large" endIcon={<SendIcon/>} onClick={handleStartQuiz}>
+      <p className={styles.scoreEndScreen}>{`Score: ${finalScore}`}</p>
+      <Button
+        className={styles.buttonEndScreen}
+        variant="outlined"
+        size="large"
+        endIcon={<SendIcon />}
+        onClick={handleStartQuiz}
+        sx={{
+          borderRadius: "5px",
+          marginBottom: "2rem",
+          fontSize: "1.8rem",
+        }}
+      >
         Start a new Quiz
       </Button>
     </Container>
